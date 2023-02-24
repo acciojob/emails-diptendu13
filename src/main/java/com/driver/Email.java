a356076860910328de552c680612e82a50b0d465
diptendu <mandaldiptendu99@gmail.com>
2023-02-24 16:51:23 +0530

    First Commit

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,29 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        boolean flagUp = false, flagLow = false, flagDig = false, flagSpcl = false;
+        if (this.password.equals(oldPassword)){
+            if (newPassword.length() >= 8){
+                for (int i=0; i<newPassword.length(); i++){
+                    char chr = newPassword.charAt(i);
+                    if (chr >= 65 && chr <= 90){
+                        flagUp = true;
+                    }
+                    else if (chr >= 97 && chr <= 122){
+                        flagLow = true;
+                    }
+                    else if (chr >= 48 && chr <=57){
+                        flagDig = true;
+                    }
+                    else{
+                        flagSpcl = true;
+                    }
+                    if (flagUp && flagLow && flagDig && flagSpcl){
+                        this.password = newPassword;
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
